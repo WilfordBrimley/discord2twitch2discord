4ed38ce29c0f6b0afc2c05edeb8069d926646efc
Lovecraft <jthenderson84@gmail.com>
2020-03-25 08:12:22 -0400

    Cleaned a bit

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 //Discord to Twitch both ways by Lovecraft#4690. WTFPL
 //Load Core config
-config = require(`./config.json`); 
+config = require(`./config.json`);
 //Discord Client
 const disBot = new(require(`discord.js`)).Client();
- disBot.once('ready', () => {
-	 console.log('Ready!');
-	 logChannel = disBot.guilds.get(config.guildMirror).channels.find(channel => channel.name === config.disChannel);
- });
- disBot.login(config.token);
- 
+disBot.once('ready', () => {
+	console.log('Ready!');
+	logChannel = disBot.guilds.get(config.guildMirror).channels.find(channel => channel.name === config.disChannel);
+});
+disBot.login(config.token);
+
 //Twitch client
 const tmi = require('tmi.js');
 const client = new tmi.Client({
-	options: { debug: true },
+	options: {
+		debug: true
+	},
 	connection: {
 		reconnect: true,
 		secure: true
@@ -21,22 +23,22 @@ const client = new tmi.Client({
 		username: config.username,
 		password: config.oauth
 	},
-	channels: [ config.channels ]
+	channels: [config.channels]
 });
 client.connect();
 
 // Handle discord to twitch
 disBot.on(`message`, (message) => {
-	if(message.author.bot == true) return;
-    if (message.channel == logChannel) {
+	if (message.author.bot == true) return;
+	if (message.channel == logChannel) {
 		client.say(config.channels, `${message.author.username}: ${message.cleanContent}`)
 	}
 })
 
 //Handle twitch to discord
 client.on('message', (channel, tags, message, self) => {
-    if(self) return;
-    logChannel.send(`${tags.username}: ${message}`)
+	if (self) return;
+	logChannel.send(`${tags.username}: ${message}`)
 });
 /* This is the config.json setup:
 {
